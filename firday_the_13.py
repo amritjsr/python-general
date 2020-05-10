75e2714083a337d6c199ea86787b6a3e5cd22e16
linux.amrit@gmail.com <linux.amrit@gmail.com>
2020-05-10 16:53:05 +0530

    First Commit

---
+++ firday_the_13.py
@@ -0,0 +1,6 @@
+'''
+This Program is to findout Firday The 13th
+'''
+import calendar
+myCalendar = calendar.TextCalendar(calendar.SUNDAY)
+# print(myCalendar.formatmonth(1999, 5)) #### PRINTING CALENDAR MAY-1999
